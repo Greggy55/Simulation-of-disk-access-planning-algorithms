8dbc69027ecee05342427ef897a75aafdda82d0f
gplawinski <mcgregor1406@gmail.com>
2025-04-04 16:50:31 +0200

    Create Disk

---
+++ src/Disk/Disk.java
@@ -0,0 +1,48 @@
+package Disk;
+
+public class Disk {
+    public final int SIZE;
+
+    private int head;
+    private String[] data;
+
+    public Disk(int size) {
+        SIZE = size;
+        head = size / 2;
+        data = new String[SIZE];
+
+        for(int i = 0; i < size; i++) {
+            data[i] = String.valueOf(i);
+        }
+    }
+
+    public boolean canMoveLeft(){
+        return head + 1 < SIZE;
+    }
+
+    public void moveLeft(){
+        head++;
+    }
+
+    public boolean canMoveRight(){
+        return head > 0;
+    }
+
+    public void moveRight(){
+        head--;
+    }
+
+    public String getData(){
+        if(head < 0 || head >= SIZE) {
+            throw new ArrayIndexOutOfBoundsException("head is out of bounds");
+        }
+        return data[head];
+    }
+
+    public void setData(String newData){
+        if(head < 0 || head >= SIZE) {
+            throw new ArrayIndexOutOfBoundsException("head is out of bounds");
+        }
+        data[head] = newData;
+    }
+}
