42b3f87e70af26669a22457da7f6d9815d1560ae
gplawinski <mcgregor1406@gmail.com>
2025-04-05 13:12:17 +0200

    add simulation fields

--- src/Simulation/Simulation.java
+++ src/Simulation/Simulation.java
@@ -1,13 +1,32 @@
 package Simulation;
 
+import Algorithms.*;
+import Strategies.*;
+
 import java.util.Random;
 
 public class Simulation {
+    private final int DISK_SIZE;
+    private final int TIME_UNIT;
+    private final int NUMBER_OF_REQUESTS;
 
     Random rnd = new Random();
 
+    private FCFS fcfs;
+    private SSTF sstf;
+    private SCAN scan;
+    private C_SCAN cScan;
 
-    public Simulation() {}
+    private EDF edf;
+    private FD_SCAN fdScan;
+
+    private int time = 0;
+
+    public Simulation(int numberOfRequests, int diskSize, int timeUnit) {
+        NUMBER_OF_REQUESTS = numberOfRequests;
+        DISK_SIZE = diskSize;
+        TIME_UNIT = timeUnit;
+    }
 
     public void start(){}
 }
